d58f4f6475ee2c53db787bbd94db823b7c95efb8
Abdulaziz2004 <abu556626@gmail.com>
2022-10-03 20:43:36 +0300

    project build

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <h1>hello</h1>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
+
+
+const Wrapper = styled.div`
+  
+`
